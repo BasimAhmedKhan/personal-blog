b9a5a9a43fd7b950a4c16b8d5d8003f4cdde5452
Abdul Rehman <ar618211@gmail.com>
2023-08-19 17:57:41 +0500

    blogs endpoints

---
+++ src/pages/api/blogs/index.js
@@ -0,0 +1,44 @@
+import { create, getAll, getByUserEmail } from "@/services/blogs";
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    const { user_email } = req.query;
+    if (user_email) {
+      try {
+        const data = getByUserEmail(user_email);
+        res.status(200).json(data)
+      } catch (e) {
+        res.status(400).json({ message: e.message });
+      }
+    } else {
+      try {
+        const data = getAll();
+        res.status(200).json(data)
+      } catch (e) {
+        res.status(500).json({ message: e.message });
+      }
+    }
+  } else if (req.method === "POST") {
+    const {
+      title,
+      content,
+      userId,
+    } = req.body;
+    if (!title || !content || !userId) {
+      res.status(400).json({ message: "Invalid body" });
+      return;
+    }
+    try {
+      const newBlog = create({
+        title,
+        content,
+        userEmail,
+      })
+      res.status(201).json(newBlog);
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
---
+++ src/services/blogs.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+
+const filePath = path.join(process.cwd(), "src", "data", "blogs.json");
+
+// schema
+// {
+//   id: 1,
+//   userEmail: "john.doe@gmail",
+//   title: "My first blog",
+//   content: "This is my first blog",
+//   createdAt: "2021-09-01T00:00:00.000Z",
+// }
+
+export function getAll () {
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+}
+
+export function create (blog) {
+  const data = getAll();
+  const lastBlog = data[data.length - 1];
+  const lastId = lastBlog.id;
+  const newBlog = {
+    ...blog,
+    id: lastId + 1,
+    createdAt: new Date().toISOString(),
+  };
+  data.push(newBlog);
+  fs.writeFileSync(filePath, JSON.stringify(data));
+  return newBlog;
+}
+
+export function getById (id) {
+  const data = getAll();
+  return data.find(p => p.id === Number(id));
+}
+
+export function getByUserEmail (userEmail) {
+  const data = getAll();
+  return data.find(p => p.userEmail.toLowerCase() === userEmail.toLowerCase());
+}
+
+export function deleteById (id) {
+  const data = getAll();
+  const index = data.findIndex(p => p.id === Number(id));
+  data.splice(index, 1);
+
+
+}
