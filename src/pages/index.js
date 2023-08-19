0398c607605806690c9929a3e5d13dedf19c1df9
Basim Ahmed Khan <kbasim039@gmail.com>
2023-08-19 18:46:20 +0500

    added dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,29 @@ export default function Dashboard({ blogs }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/icon.png" />
       </Head>
-      <section className="text-gray-600 body-font overflow-hidden marginTB b-radius">
+      <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 w-full">
+                <h1>Dashboard</h1>
+                <div class="relative">
+                  <label for="message" class="leading-7 text-sm text-gray-600">Title</label>
+                  <input type="text" id="title" name="title" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+                <div class="relative">
+                  <label for="message" class="leading-7 text-sm text-gray-600">Blog</label>
+                  <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font overflow-hidden margin b-radius">
         <div className="container px-5 py-24 mx-auto">
           {session.status === "authenticated" && (
             <div className="">
